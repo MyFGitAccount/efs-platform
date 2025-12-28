697eabf07e71e14bcf7d324ad7e04517f906c7e7
MyFGitAccount <coherentlaserbeam@gmail.com>
2025-12-28 17:29:37 +0800

    change index.js to common js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
-import app from '../server/server.js';
+/*import app from '../server/server.js';
 
 export default async (req, res) => {
+  return app(req, res);
+};*/
+// index.js   ← CommonJS version
+const app = require('../server/server.js');
+
+module.exports = (req, res) => {
   return app(req, res);
 };
